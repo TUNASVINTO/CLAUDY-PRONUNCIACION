7458374af7ea313d7524b8f4c30f34db366c4456
TUNASVINTO <tunasvintou@gmail.com>
2024-11-10 20:31:46 -0400

    primer programa

---
+++ APRENDA_A_EXPRESARSE.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Interactivo de Lectura</title>
+    <link rel="stylesheet" href="estilo.css">
+    <link rel="stylesheet" href="script.js">
+</head>
+<body background="FONDOS DE PANTALLA/IMA7.jpg">
+    <header>
+         <h1>APRENDE A EXPRESARSE SIN TEMOR AL MIEDO</h1>
+    </header>
+
+    <main>
+        <ul>
+            <li><a href="ADIVINANZAS.php">ADIVINANZAS</a></li><br><br>
+            <li><a href="CUENTOS.php">CUENTOS</a></li><br><br>
+            <li><a href="CANCIONES_INFANTILES.php">CANSIONES INFANTILES</a></li><br><br>
+            <li><a href="LEYENDAS_Y_MITOS.php">LEYENDAS Y MITOS</a></li><br><br>
+            <li><a href="CANTOS_Y_POEMAS_EPTICOS.php">CANTOS Y POEMAS EPTICOS</a></li><br><br>
+            <li><a href="REPRESENTACIONES_DRAMATICAS.php">REPRESENTACIONES DRAMATICAS</a></li><br><br><br>
+            <li><a href="PAGINA2.php">VOLVER ATRAS</a></li><br><br><br><br>
+            <li><a href="index.php">SALIR</a></li>
+        </ul>
+    </main>
+
+    <footer>
+       
+    </footer>
+
+    <script src="script.js"></script>
+</body>
+</html>
